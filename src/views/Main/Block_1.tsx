6da755dbaa2b911e0f16694cea11c3839067142e
Vladislav Li <vladislav.li911@gmail.com>
2023-05-10 23:48:06 +0900

    1

--- src/views/Main/Block_1.tsx
+++ src/views/Main/Block_1.tsx
@@ -3,21 +3,7 @@ import React from "react";
 import Header from "@/components/Layout/Header";
 
 function Block_1() {
-  return (
-    <header className="h-[100vh] w-full">
-      <div className="absolute top-0 h-full w-full bg-black/60" />
-      <video
-        autoPlay
-        className="h-full w-full object-cover"
-        loop
-        muted
-        src="video.mp4"
-      />
-      <div className="absolute top-0 flex h-10 w-full items-center bg-black/30">
-        AB Koreaa
-      </div>
-    </header>
-  );
+  return <></>;
 }
 
 export default Block_1;
