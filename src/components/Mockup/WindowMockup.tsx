c92846511c6f54e8c3b9fa5589ada09fa9642a08
Vladislav Li <vladislav.li911@gmail.com>
2025-04-16 09:15:12 +0500

    Refactor imports and format code for consistency

--- src/components/Mockup/WindowMockup.tsx
+++ src/components/Mockup/WindowMockup.tsx
@@ -1,80 +1,81 @@
-import React, { forwardRef, type JSX } from "react";
+import React, {forwardRef, type JSX} from "react";
 import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
+import {twMerge} from "tailwind-merge";
 
-import { bgColors, brandColors, componentStatuses } from "@/components/@types";
+import {bgColors, brandColors, componentStatuses} from "@/components/@types";
 
 export const windowMockupColors = [
-  ...bgColors,
-  ...brandColors,
-  ...componentStatuses,
+    ...bgColors,
+    ...brandColors,
+    ...componentStatuses,
 ] as const;
 
 type WindowMockupColors = (typeof windowMockupColors)[number];
 
 export type WindowMockupProps = React.HTMLAttributes<HTMLDivElement> & {
-  frameColor?: WindowMockupColors;
-  backgroundColor?: WindowMockupColors;
-  border?: boolean;
-  borderColor?: WindowMockupColors;
+    frameColor?: WindowMockupColors;
+    backgroundColor?: WindowMockupColors;
+    border?: boolean;
+    borderColor?: WindowMockupColors;
 };
 
 const WindowMockup = forwardRef<HTMLDivElement, WindowMockupProps>(
-  (
-    {
-      border,
-      borderColor,
-      backgroundColor,
-      frameColor,
-      className,
-      children,
-      ...props
-    },
-    ref
-  ): JSX.Element => {
-    const borderColorValue = borderColor
-      ? borderColor
-      : frameColor || "base-300";
+    (
+        {
+            border,
+            borderColor,
+            backgroundColor,
+            frameColor,
+            className,
+            children,
+            ...props
+        },
+        ref
+    ): JSX.Element => {
+        const borderColorValue = borderColor
+            ? borderColor
+            : frameColor || "base-300";
 
-    const classes = twMerge(
-      "mockup-window",
-      border && `border border-${borderColorValue}`,
-      clsx({
-        [`border-${borderColorValue}`]: borderColorValue,
-        [`bg-${frameColor}`]: frameColor,
-      }),
-      className
-    );
+        const classes = twMerge(
+            "mockup-window",
+            border && `border border-${borderColorValue}`,
+            clsx({
+                [`border-${borderColorValue}`]: borderColorValue,
+                [`bg-${frameColor}`]: frameColor,
+            }),
+            className
+        );
 
-    const numChildren = React.Children.count(children);
-    const firstChild = numChildren > 0 && React.Children.toArray(children)[0];
+        const numChildren = React.Children.count(children);
+        const firstChild = numChildren > 0 && React.Children.toArray(children)[0];
 
-    const innerClasses = clsx(
-      backgroundColor && `bg-${backgroundColor}`,
-      border && `border- border-t${borderColorValue}`,
-      "p-4"
-    );
+        const innerClasses = clsx(
+            backgroundColor && `bg-${backgroundColor}`,
+            border && `border- border-t${borderColorValue}`,
+            "p-4"
+        );
 
-    const innerEl =
-      firstChild && React.isValidElement(firstChild) ? (
-        React.cloneElement(firstChild as React.ReactElement<any>, {
-          className: twMerge(innerClasses, firstChild.props.className),
-        })
-      ) : (
-        <div className={innerClasses}>{children}</div>
-      );
 
-    return (
-      <div
-        aria-label="Window mockup"
-        {...props}
-        className={classes}
-        ref={ref}
-      >
-        {innerEl}
-      </div>
-    );
-  }
+        const innerEl =
+            !(firstChild && React.isValidElement(firstChild)) ? (
+                <div className={innerClasses}>{children}</div>
+            ) : (
+                React.cloneElement(firstChild as React.ReactElement<any>, {// @ts-ignore
+                    className: twMerge(innerClasses, firstChild.props.className),
+                })
+            );
+
+        return (
+            <div
+                aria-label="Window mockup"
+                {...props}
+                className={classes}
+                ref={ref}
+            >
+                {innerEl}
+            </div>
+        );
+    }
 );
 
 WindowMockup.displayName = "WindowMockup";
